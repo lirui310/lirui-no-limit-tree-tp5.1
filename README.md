cc692614baf2668170f2ad9142e6e25b6915f496
lirui <1950767658@qq.com>
2020-03-29 18:26:33 +0800

    开发测试完成

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # lirui-no-limit-tree-tp5.1
 基于php5.1开发 无限制Tree上下级关系绑定 可避免使用递归 
 
-
 ## 如何安装？
 `1.安装thinkphp5.1 配置好数据库连接`
 
 `2.composer require lirui/no-limit-tree-tp5.1`
 
-`3.初始化安装表 php think no-limit-tree:init 默认表名：no_limit_tree 
-如果需要自定义表 php think no-limit-tree:init table_name`
+`3.初始化安装表 php think no-limit-tree:init 默认表名：no_limit_tree`
+
+`如果需要自定义表 php think no-limit-tree:init table_name`
+
 `测试完成后需要重新上线部署 也是运行此命令 会自动删除表 重新建立`
 
 ## 如何使用？
@@ -16,4 +17,27 @@
 
 `自定义表：$obj = new \lirui\NoLimitTree\Service(table_name)`
 
-`添加节点：$obj->add(uid, pid)`
+`添加节点：$obj->add(uid, pid) 返回true|false`
+
+`查看某个节点下的所有子节点 包含自己这个节点：$obj->getSubChild(uid, limit) limit 可选：0,10 自己分页`
+
+`统计某个节点下的所有子节点 包含自己这个节点：$obj->countSubChild(uid) 返回总数量`
+
+`查看某个节点下的所有子节点 不包含自己这个节点：$obj->getSubChildNoMe(uid, limit) limit 可选：0,10 自己分页`
+
+`统计某个节点下的所有子节点 不包含自己这个节点：$obj->countSubChildNoMe(uid) 返回总数量`
+
+`获取所有叶子节点(最下面一级)：$obj->getLeafChild() 最下一层的所有节点uid`
+
+`获取某个节点的所有父级uid 包含自己这个节点和系统预设顶级节点uid=0：$obj->getParentIds()`
+
+`获取某个节点下的节点 可添加深度条件(可选) 包含自己这个节点：$obj->getChildDepth(uid, depth, limit) 可选：0,10 自己分页`
+
+`统计某个节点下的节点 可添加深度条件(可选) 包含自己这个节点：$obj->countChildDepth(uid, depth) 返回总数量`
+
+## 有问题请联系
+QQ：1950767658
+
+微信：wxmm686800
+
+承接各种系统咨询和开发
--- src/Service.php
+++ src/Service.php
@@ -63,17 +63,16 @@ class Service extends Base
     }
 
     /**
-     * 查看某个节点下的所有子节点 包含自己这个节点
+     * @description 查看某个节点下的所有子节点 包含自己这个节点
      * @param int $uid 节点id，默认是顶级节点0
      * @param string $limit 每次取出数量
      * @return array                所有节点的id（fid）
      */
-    public function getSubChild(int $uid = 0, $limit = ""): array
+    public function getSubChild(int $uid = 0, string $limit = ""): array
     {
         //排除自己? AND node.uid <> $uid
-        $table = $this->table;
-        $uid = 6;
-        $sql = "SELECT node.uid,node.uname FROM {$table} AS node,{$table} AS parent WHERE node.lft BETWEEN parent.lft AND parent.rgt AND parent.uid = {$uid} ORDER BY node.lft";
+        $table = self::$tableName;
+        $sql = "SELECT node.uid FROM {$table} AS node,{$table} AS parent WHERE node.lft BETWEEN parent.lft AND parent.rgt AND parent.uid = {$uid} ORDER BY node.lft";
         if ($limit > 0) {
             $sql .= " LIMIT $limit";
         }
@@ -81,62 +80,78 @@ class Service extends Base
         return $res;
     }
 
-    public function getSubChildNoMe($uid = 0, $limit = "")
+    /**
+     * @param int $uid 节点id，默认是顶级节点0
+     * @return int
+     */
+    public function countSubChild(int $uid = 0): int
+    {
+        $table = self::$tableName;
+        $sql = "SELECT COUNT(node.uid) AS c FROM {$table} AS node,{$table} AS parent WHERE node.lft BETWEEN parent.lft AND parent.rgt AND parent.uid = {$uid} ORDER BY node.lft";
+        $res = Db::query($sql);
+        return $res[0]['c'];
+    }
+
+    public function getSubChildNoMe(int $uid = 0, string $limit = "")
     {
         //排除自己? AND node.uid <> $uid
-        $table = $this->table;
-        $uid = 1;
-        $sql = "SELECT node.uid,node.uname FROM {$table} AS node,{$table} AS parent WHERE node.lft BETWEEN parent.lft AND parent.rgt AND parent.uid = {$uid} ORDER BY node.lft";
+        $table = self::$tableName;
+        $sql = "SELECT node.uid FROM {$table} AS node,{$table} AS parent WHERE node.lft BETWEEN parent.lft AND parent.rgt AND parent.uid = {$uid} AND node.uid <> {$uid} ORDER BY node.lft";
         if ($limit > 0) {
             $sql .= " LIMIT $limit";
         }
         $res = Db::query($sql);
+        return $res;
+    }
 
-        var_dump($res);
-        die;
+    public function countSubChildNoMe(int $uid = 0): int
+    {
+        //排除自己? AND node.uid <> $uid
+        $table = self::$tableName;
+        $sql = "SELECT COUNT(node.uid) AS c FROM {$table} AS node,{$table} AS parent WHERE node.lft BETWEEN parent.lft AND parent.rgt AND parent.uid = {$uid} AND node.uid <> {$uid} ORDER BY node.lft";
+        $res = Db::query($sql);
+        return $res[0]['c'];
     }
 
     /**
-     * 获取所有叶子节点(最下面一级)
-     * @return array     返回fid
+     * @description 获取所有叶子节点(最下面一级)
+     * @return array   返回uid
      */
-    public function getLeafChild()
+    public function getLeafChild(): array
     {
-        $table = $this->table;
+        $table = self::$tableName;
         $sql = "SELECT uid FROM {$table} WHERE rgt = lft + 1";
         $res = Db::query($sql);
-        var_dump($res);
+        return $res;
     }
 
     /**
-     * 获取某个节点的所有父级uid
-     * @param type $fid
+     * @description 获取某个节点的所有父级uid
+     * @param int $uid
+     * @return array 返回uid
      */
-    public function getParentIds($uid = 0)
+    public function getParentIds(int $uid = 0): array
     {
-        $table = $this->table;
-        $uid = 1018;
+        $table = self::$tableName;
         if ($uid == 0) {
             return [0];
         }
         $sql = "SELECT parent.uid FROM {$table} AS node,{$table} AS parent WHERE node.lft BETWEEN parent.lft AND parent.rgt AND node.uid = {$uid} ORDER BY parent.lft";
         $res = Db::query($sql);
-        var_dump($res);
+        return $res;
     }
 
     /**
-     * 获取某个节点下的节点深度
-     * @param type $uid $uid 为第一级 0
-     * @param type $depth 获取深度，默认返回所有
-     * @param int $limit 每次取出多少个，默认为""，则取出所有
-     * @return type
+     * @description 获取某个节点下的节点 可添加深度条件
+     * @param int $uid $uid 为第一级 0
+     * @param int $depth 获取深度，默认返回所有
+     * @param string $limit 每次取出多少个，默认为""，则取出所有
+     * @return array
      */
-    public function getChildDepth($uid = 0, $depth = 0, $limit = "")
+    public function getChildDepth(int $uid = 0, int $depth = 0, string $limit = ""): array
     {
-        $table = $this->table;
+        $table = self::$tableName;
         $orderBy = "node.lft";
-        $uid = 6;
-        //$depth = 1;
         if ($uid == 0) {
             $sql = "SELECT node.uid, (COUNT(parent.uid) - 1) AS depth FROM {$table} AS node,{$table} AS parent WHERE node.lft BETWEEN parent.lft AND parent.rgt GROUP BY node.uid ORDER BY node.lft";
         } else {
@@ -154,6 +169,31 @@ class Service extends Base
             $sql .= " LIMIT $limit";
         }
         $res = Db::query($sql);
-        var_dump($res);
+        return $res;
+    }
+
+    /**
+     * @description 统计某个节点下的节点 可添加深度条件
+     * @param int $uid $uid 为第一级 0
+     * @param int $depth 获取深度，默认返回所有
+     * @return int
+     */
+    public function countChildDepth($uid = 0, $depth = 0): int
+    {
+        $table = self::$tableName;
+        if ($uid == 0) {
+            $sql = "SELECT node.uid, (COUNT(parent.uid) - 1) AS depth FROM {$table} AS node,{$table} AS parent WHERE node.lft BETWEEN parent.lft AND parent.rgt GROUP BY node.uid ORDER BY node.lft";
+        } else {
+            $sql = "SELECT node.uid, (COUNT(parent.uid) - (sub_tree.depth + 1)) AS depth FROM {$table} AS node,{$table} AS parent,{$table} AS sub_parent,
+                (SELECT node.uid, (COUNT(parent.uid) - 1) AS depth FROM {$table} AS node, {$table} AS parent WHERE node.lft BETWEEN parent.lft AND parent.rgt
+                AND node.uid = {$uid} GROUP BY node.uid)
+                AS sub_tree WHERE node.lft BETWEEN parent.lft AND parent.rgt AND node.lft BETWEEN sub_parent.lft AND sub_parent.rgt
+                AND sub_parent.uid = sub_tree.uid GROUP BY node.uid";
+            if ($depth > 0) {
+                $sql .= " HAVING depth <= {$depth}";
+            }
+        }
+        $res = Db::query($sql);
+        return count($res);
     }
 }
